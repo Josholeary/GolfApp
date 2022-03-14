bcd4b2690f93dfe065ee6c83f1039a1d2e983e93
Josh OLeary <josholeary7@gmail.com>
2022-03-14 20:00:17 +0000

    Added database

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+from enum import unique
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import PrimaryKeyConstraint
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///golf.db'
+db=SQLAlchemy(app)
+
+class game(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    player_name = db.Column(db.String(length=40), nullable=False)
+    hole = db.Column(db.Integer(), nullable=False)
+    score = db.Column(db.Integer(), nullable=False)
+
+
+
 
 @app.route('/')
 @app.route('/index')
