2043ce592e205f93cb1c8ef97707f5c02d3a1d2e
Josh OLeary <josholeary7@gmail.com>
2022-03-14 18:34:00 +0000

    Added low level flask functionality and base styling

--- app.py
+++ app.py
@@ -3,8 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/start')
+def start():
+    return render_template('start.html', num_players = 0)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
