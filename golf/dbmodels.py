7ec5eb9ed143b0efae7d4e78cd4fe1a80852b16d
Josh OLeary <josholeary7@gmail.com>
2022-03-15 13:58:04 +0000

    Added backend queries and join.html

--- golf/dbmodels.py
+++ golf/dbmodels.py
@@ -4,8 +4,9 @@ from enum import unique
 
 class setup(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    session_code = db.Column(db.Integer(), nullable=False, unique=True)
-    owner_name = db.Column(db.String(length=40), nullable=False)
+    host_name = db.Column(db.String(length=40), nullable=False)
+    num_holes = db.Column(db.Integer(), nullable=False)
+    session_password = db.Column(db.String(), nullable=False, unique=True)
     players = db.relationship('player', backref='joined_game', lazy=True)
 
 class player(db.Model):
@@ -13,7 +14,7 @@ class player(db.Model):
     player_name = db.Column(db.String(length=40), nullable=False)
     hole_num = db.Column(db.Integer(), nullable=False)
     score = db.Column(db.Integer(), nullable=False)
-    joinCode = db.Column(db.Integer(), db.ForeignKey('setup.session_code'))
+    join_code = db.Column(db.Integer(), db.ForeignKey('setup.session_code'))
 
     def __repr__(self):
         return f'item {self.name}'
--- golf/pathways.py
+++ golf/pathways.py
@@ -1,14 +1,28 @@
+from crypt import methods
+from wsgiref import validate
 from golf import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from golf.dbmodels import setup, player
 from golf.forms import SetupForm
+from golf import db
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/setup')
-def setup():
+@app.route('/setup', methods=['GET','POST'])
+def setup_page():
     form = SetupForm()
+    if form.validate_on_submit():
+        setup_to_create = setup(host_name=form.host_name.data,
+                                 num_holes=form.num_holes.data,
+                                 session_password=form.session_password.data)
+        db.session.add(setup_to_create)
+        db.session.commit
+        #return redirect(url_for('game'))
     return render_template('setup.html', form=form)
+
+@app.route('/join')
+def join_page():
+    return render_template('join.html')
